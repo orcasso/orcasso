b9fe6a71d03e0366f0c80c897e64d283ddfdfeb0
Benjamin <benito103e@users.noreply.github.com>
2025-03-08 21:47:47 +0100

    Configure Select2 for global instanciation

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,28 @@
 import './styles/app.css';
+
+window.initializeSelect2s = function () {
+    $('.select2-ajax-loader').select2({
+        ajax: {}
+    });
+
+    let select2Selects = $('.select2-select');
+    if (select2Selects.length > 0) {
+        select2Selects.each(function () {
+            let initialized = $(this).data('select2');
+
+            if (!initialized) {
+                $(this).select2({
+                    escapeMarkup: function(markup) {
+                        return markup;
+                    }
+                });
+            }
+        });
+    }
+};
+
+
+$(document).ready(function () {
+    $.fn.select2.defaults.set('theme', 'bootstrap4');
+    window.initializeSelect2s();
+});
